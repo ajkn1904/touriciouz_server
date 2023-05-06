5ce863e90f2ae5ada45359018042a64ce591a15f
ajkn1904 <ajknishat@gmail.com>
2023-05-07 02:54:02 +0600

    mongodb added & api for loading packages added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 
 
 const app = express();
@@ -9,6 +11,39 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7splzic.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+
+    await client.connect();
+    const packagesCollection = client.db('touriciouz').collection('packages');
+
+
+
+
+    try{
+
+        app.get('/packages', async (req, res) => {
+            const query = {}
+            const result = await packagesCollection.find(query).toArray()
+            res.send(result)
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
+
+
 
 
 
