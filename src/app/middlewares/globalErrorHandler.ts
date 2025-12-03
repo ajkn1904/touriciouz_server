1fb97071bfadb106766dbca7ab43970710a2f9e4
ajkn1904 <ajknishat@gmail.com>
2025-12-04 02:13:32 +0600

    config & error handlers added & initial server setup done

---
+++ src/app/middlewares/globalErrorHandler.ts
@@ -0,0 +1,65 @@
+import { Request, Response, NextFunction } from "express";
+import httpStatus from "http-status-codes";
+
+const globalErrorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.log(err);
+
+  let statusCode = httpStatus.INTERNAL_SERVER_ERROR;
+  let message = "Something went wrong!";
+  let error = err;
+
+  
+  // Prisma known errors
+  // Duplicate
+  if (err.code === "P2002") {
+    statusCode = httpStatus.CONFLICT;
+    message = "Duplicate key error";
+    error = err.meta;
+  }
+
+  // FK constraint
+  if (err.code === "P2003") {
+    statusCode = httpStatus.BAD_REQUEST;
+    message = "Foreign key constraint failed";
+    error = err.meta;
+  }
+
+  if (err.code === "P1000") {
+    statusCode = httpStatus.BAD_GATEWAY;
+    message = "Authentication failed against database server";
+    error = err.meta;
+  }
+
+  // Validation errors
+  if (err.name === "PrismaClientValidationError") {
+    statusCode = httpStatus.BAD_REQUEST;
+    message = "Validation Error";
+    error = err.message;
+  }
+
+  // Initialization / unknown errors
+  if (err.name === "PrismaClientUnknownRequestError") {
+    statusCode = httpStatus.BAD_REQUEST;
+    message = "Unknown Prisma error occurred!";
+    error = err.message;
+  }
+
+  if (err.name === "PrismaClientInitializationError") {
+    statusCode = httpStatus.BAD_REQUEST;
+    message = "Prisma client failed to initialize!";
+    error = err.message;
+  }
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    error,
+  });
+};
+
+export default globalErrorHandler;
