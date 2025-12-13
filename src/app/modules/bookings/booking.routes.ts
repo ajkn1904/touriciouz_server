17e3f1f8e8d8b6815a03b520e067edceb89ff67e
ajkn1904 <ajknishat@gmail.com>
2025-12-13 21:05:55 +0600

    updated

--- src/app/modules/bookings/booking.routes.ts
+++ src/app/modules/bookings/booking.routes.ts
@@ -9,12 +9,12 @@ const router = Router();
 
 router.post("/", checkAuth(UserRole.TOURIST), validationRequest(createBookingZodSchema), BookingController.createBooking);
 router.get("/", checkAuth(UserRole.ADMIN), BookingController.getAllBookings);
-router.get("/my-bookings", checkAuth(...Object.values(UserRole)), BookingController.getUserBookings);
 router.get(
   "/guide/my-bookings",
   checkAuth(UserRole.GUIDE),
   BookingController.getGuideBookings
 );
+router.get("/my-bookings", checkAuth(...Object.values(UserRole)), BookingController.getUserBookings);
 router.get("/:id", checkAuth(...Object.values(UserRole)), BookingController.getBookingById);
 router.patch("/status/:id", checkAuth(...Object.values(UserRole)), validationRequest(updateBookingStatusZodSchema), BookingController.updateBookingStatus);
 
